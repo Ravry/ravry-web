62d79fc241d9e402271ef2d63dc63c89f588d505
ravry <paulloibl1@gmail.com>
2025-05-23 14:39:24 +0200

    minor improvements

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,12 +2,21 @@ import Selector from "./Selector";
 
 export default function Content() {
     return (
-        <div className="flex flex-col items-center w-full text-center gap-2">
+        <div className="flex flex-col items-center w-full text-center gap-3">
             <p className="text-6xl font-bold">View my work</p>
             <p className="text-xl">Discover the projects and experiences behind my journey.</p>
             <div className="foreground p-4 rounded-2xl">Browse my work</div>
             <p>v1.0.1</p>
             <Selector/>
+
+            <p className="text-6xl font-bold mt-10">Contact Me</p>
+            <p className="text-xl">Contact me on various platforms.</p>
+            <div className="flex flex-row w-full items-center justify-center text-3xl gap-5">
+                <div><i className="fa-brands fa-instagram"></i></div>
+                <div><i className="fa-brands fa-x-twitter"></i></div>
+                <div><i className="fa-brands fa-bluesky"></i></div>
+            </div>
+            &nbsp;
         </div>
     );
 }
