14ab4ddd7cb53431f662690092f3dbf415b9c89b
ravry <paulloibl1@gmail.com>
2025-05-24 20:35:04 +0200

    minor adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 export default function Footer() {
-    console.log("Footer rendered"); // Add this
     return (
         <div className="flex flex-row justify-center w-full h-auto">
             <span className="flex items-center rounded-xl p-4 font-bold foreground shadow-xl hover:cursor-pointer hover:cursor-pointer" onClick={() => document.getElementById("background")?.scrollTo({ top: 0, behavior: 'smooth'})}>
