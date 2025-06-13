d15d4ffd70de15a83c76c63f743f8a5a0018460e
ravry <paulloibl1@gmail.com>
2025-06-13 20:31:00 +0200

    added an animated notification

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import Notification from "./Notification";
 
 export default function App() {
     return (
@@ -8,6 +9,8 @@ export default function App() {
             {/* Parallax background */}
             <div className="absolute top-0 left-0 w-full h-full z-0 bg-fixed bg-cover bg-center">
             </div>
+
+            <Notification />
 
             {/* Foreground content */}
             <div className="relative z-10 flex flex-col gap-5 p-2 min-h-screen backdrop-blur-sm">
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,8 +1,37 @@
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
 export default function Notification() {
+    const [isVisible, setIsVisible] = useState(true);
+
     return (
-        <div>
-            <h1>title</h1>
-            <p>message</p>
-        </div>
+        <AnimatePresence>
+            {isVisible && (
+                <motion.div
+                    initial={{ y: '100%', opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: '100%', opacity: 0 }}
+                    transition={{ type: 'spring', stiffness: 100, damping: 15 }}
+                    className="fixed flex flex-col justify-center items-center z-50 w-full h-full backdrop-blur-xl"
+                >
+                    <div className="flex flex-col gap-5 w-100 p-5">
+                        <h1 className="text-2xl font-bold">
+                            🚀 Contribute to My Project!
+                        </h1>
+                        <p>
+                            Hey everyone! I'm working on an open-source project called Glint – and I’d love your help! If you're interested in contributing, check it out on GitHub: <a href="https://github.com/Ravry/glint" className="underline text-blue-500">https://github.com/Ravry/glint</a>.<br />
+                            <br />
+                            Whether you're into coding, testing, documentation, or just want to give feedback, every bit helps.✨
+                        </p>
+                        <button
+                            onClick={() => setIsVisible(false)}
+                            className="px-4 py-2 bg-blue-600 text-white rounded-xl shadow-xl hover:bg-blue-700 transition"
+                        >
+                            Ok!
+                        </button>
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 }
